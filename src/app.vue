e122fb93a6b3f2b3a743417d9cafbd057fd85f31
Marco Klein <marco.klein94@gmail.com>
2019-05-22 15:03:29 +0200

    switching to vue

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    div {{testString}}
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            testString: 'hallo'
+        }
+    }
+});
+</script>
+
