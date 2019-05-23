f123c186d34f5bf3ed4ae9ad4fd339e9d8ec7d1b
Marco Klein <marco.klein94@gmail.com>
2019-05-23 12:51:12 +0200

    include bootstrap

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,38 +1,11 @@
-import Peer, { DataConnection } from 'peerjs';
+import Vue from 'vue';
+import App from './server.vue';
+import './server.scss';
 
-console.log('Creating new peer.');
-let peer = new Peer('CATCHME2TFT');
-
-peer.on('open', (id) => {
-    console.log('Peer ready with id: ', id);
-});
-
-peer.on('connection', (conn) => {
-    // measure ping continuously
-    let pingStart: number;
-    let measurePing = () => {
-        console.log('send ping');
-        pingStart = Date.now();
-        conn.send('ping');
-        setTimeout(() => {
-            measurePing();
-        }, 1000);
-    }
-
-    conn.on('data', (data) => {
-        console.log('received data: ', data);
-        if (data === 'pong') {
-            // stop ping measure
-            console.log('received pong');
-            let ping = Date.now() - pingStart;
-            console.log('ping: ' + ping);
-            conn.send({t: 'ping', v: ping});
-        } else if (data === 'ping') {
-            conn.send('pong');
-        }
-    });
-    console.log('Another peer connected!', conn.peer);
-    console.log('Sending welcome messsage.');
-    conn.send('Welcome');
-    measurePing();
+/**
+ * Entry for Vue application.
+ */
+new Vue({
+    el: '#app',
+    render: h => h(App)
 });
---
+++ src/server/server.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div
+  h1 Test Server of Smartphone Gamepad library
+  button.btn.btn-primary(type='button') Start
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { SmartPadServer } from './SmartPadServer';
+
+@Component
+export default class TestServerApp extends Vue {
+    server: SmartPadServer;
+    mounted() {
+        // initialize
+        this.server = new SmartPadServer();
+    }
+
+}
+</script>
+
+<style>
+</style>
+
