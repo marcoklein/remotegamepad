3e4a1e98dc8d04be2db12d063a18ab1bf5c871f0
Marco Klein <marco.klein94@gmail.com>
2019-05-25 19:11:22 +0200

    overlay with fullscreen button

--- src/Overlay.vue
+++ src/Overlay.vue
@@ -1,24 +1,58 @@
 <template lang="pug">
-h2 super overlay
+div#overlayContainer
+  h2 super overlay
+    input#fullscreenButton.overlay(src='assets/buttonExpand.png', type='image', alt='Fullscreen', v-on:click="toogleFullscreen", v-if="fullscreenEnabled && !isFullscreen")
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import screenfull, { Screenfull } from 'screenfull';
+let fullscreenPlugin: Screenfull = <Screenfull> screenfull;
+
 
 @Component
 export default class Overlay extends Vue {
+
+    beforeCreate() {
+        // change listener of fullscreen plugin
+        fullscreenPlugin.on('change', () => {
+            this.$data.isFullscreen = fullscreenPlugin.isFullscreen;
+        });
+    }
+
+    mounted() {
+    }
+
+    /**
+     * Use screenfull.js to toggle fullscreen if available.
+     */
+    toogleFullscreen() {
+        fullscreenPlugin.toggle();
+    }
+
+    data() {
+        return {
+            fullscreenEnabled: fullscreenPlugin.enabled,
+            isFullscreen: fullscreenPlugin.isFullscreen
+        }
+    }
+
+
 
 }
 </script>
 
 <style>
-
+#overlayContainer {
+    width: 100%;
+    height: 100%;
+}
 /* Show fullscreenButton on top */
 #fullscreenButton {
     position: absolute;
-    top: 5%;
-    right: 5%;
+    top: 16px;
+    right: 16px;
     cursor: pointer;
 }
 </style>
