60c32a4af2e1c35accd4fdc6c374136047e4d453
Marco Klein <marco.klein94@gmail.com>
2019-05-27 19:38:36 +0200

    merged overlay into main

--- src/client/Main.vue
+++ src/client/Main.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
 div
-  Overlay.overlay(v-bind:network='network')
+  // overlay
+  div#overlayContainer
+    h4(v-if='network') {{ network.averagePing }}
+    // fullscreen button
+    input#fullscreenButton.overlay(src='../assets/buttonExpand.png', type='image', alt='Fullscreen', v-on:click="toogleFullscreen", v-if="fullscreenEnabled && !isFullscreen")
+  
+    // alerts
+    div#alertContainer.overlay
+      b-alert(:show='dismissCountDown', dismissible='', variant='warning', @dismissed='dismissCountDown=0', @dismiss-count-down='countDownChanged')
+        p {{ alertWarningMessage }}
+        b-progress(variant='warning', :max='dismissSecs', :value='dismissCountDown', height='4px')
+
+  // images
   #images(style='display: none')
     // Loaded images for canvas
     img#padDarkImage(src='../assets/padDark.png')
     img#padBackgroundDarkImage(src='../assets/padBackgroundDark.png')
     img#buttonAImage(src='../assets/buttonA.png')
     
-  // alerts
-  b-alert(:show='dismissCountDown', dismissible='', variant='warning', @dismissed='dismissCountDown=0', @dismiss-count-down='countDownChanged')
-    p {{ alertWarningMessage }}
-    b-progress(variant='warning', :max='dismissSecs', :value='dismissCountDown', height='4px')
   // canvas for rendering
   canvas#gamepadCanvas
 
@@ -22,23 +30,28 @@ div
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Application } from './Application';
-import Overlay from './Overlay.vue';
+import screenfull, { Screenfull } from 'screenfull';
 import { SmartPadClient } from './SmartPadClient';
+let fullscreenPlugin: Screenfull = <Screenfull> screenfull;
 
 
-@Component({
-    components: {
-        Overlay
-    }
-})
+@Component
 export default class Main extends Vue {
     application: Application;
 
     network: SmartPadClient = null;
     dismissCountDown = 0;
-    dismissSecs = 5;
+    dismissSecs = 200;
     alertWarningMessage = '';
+    fullscreenEnabled = fullscreenPlugin.enabled;
+    isFullscreen = fullscreenPlugin.isFullscreen;
 
+    beforeCreate() {
+        // change listener of fullscreen plugin
+        fullscreenPlugin.on('change', () => {
+            this.$data.isFullscreen = fullscreenPlugin.isFullscreen;
+        });
+    }
     mounted() {
         // initialize
         let canvas = <HTMLCanvasElement> document.getElementById('gamepadCanvas');
@@ -68,6 +81,12 @@ export default class Main extends Vue {
         this.dismissCountDown = dismissCountDown
     }
 
+    /**
+     * Use screenfull.js to toggle fullscreen if available.
+     */
+    toogleFullscreen() {
+        fullscreenPlugin.toggle();
+    }
 }
 </script>
 
@@ -87,9 +106,27 @@ html, body {
 .overlay {
     position: absolute;
 }
-b-alert {
+#alertContainer {
     position: absolute;
     top: 0px;
     left: 0px;
+    width: 100%;
+}
+#overlayContainer {
+    width: 100%;
+    height: 100%;
+    /* Click through container */
+    pointer-events: none;
+}
+#overlayContainer > * {
+    /* Default click behavior for childs. */
+    pointer-events: initial;
+}
+/* Show fullscreenButton on top */
+#fullscreenButton {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    cursor: pointer;
 }
 </style>
