1bdbf13ce43b0164ec610c5cff34e8f3c8b3302a
Marco Klein <marco.klein94@gmail.com>
2019-05-31 14:24:02 +0200

    testing for preexisting remoteGamepadAPI in global

--- src/serverlib.ts
+++ src/serverlib.ts
@@ -1,12 +1,14 @@
 import { NetworkGamepadAPIClass } from "./server/RemoteGamepadAPI";
 
-const remoteGamepadAPI = NetworkGamepadAPIClass.getInstance();
 
 // add remote gamepad api to global object...
 let root: any = typeof self == 'object' && self.self === self && self ||
             typeof global == 'object' && global.global === global && global ||
             this ||
             {};
+
+// use existing remote gamepad api or load new instance
+const remoteGamepadAPI = root.remoteGamepadAPI || NetworkGamepadAPIClass.getInstance();
 root.remoteGamepadAPI = remoteGamepadAPI;
 
 export { remoteGamepadAPI };
