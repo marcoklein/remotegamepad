70756f7540f779bc23c2a8a83b31cc4276ea55c2
Marco Klein <marco.klein94@gmail.com>
2019-06-11 17:47:07 +0200

    send less axis updates

--- src/client/Application.ts
+++ src/client/Application.ts
@@ -55,11 +55,16 @@ export class Application {
         let pad = new Pad();
         pad.positionRelative.set(0.2, 0.7);
         this.addUiElement(pad);
+        let lastAxisUpdate = 0;
         pad.events.on('axisUpdate', (axis: {x: number, y: number}) => {
-            this.network.sendMessage('axisUpdate', {
-                index: 0,
-                axis: axis
-            })
+            // only send axis update every 30 ms
+            if (axis.x !== 0 && axis.y !== 0 && Date.now() - lastAxisUpdate > 30) {
+                lastAxisUpdate = Date.now();
+                this.network.sendMessage('axisUpdate', {
+                    index: 0,
+                    axis: axis
+                });
+            }
         });
         // add button start
         let buttonStart = new Button(<HTMLImageElement> document.getElementById('buttonStartImage'));
